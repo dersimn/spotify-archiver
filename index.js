72689bc7157e83897904a0b7969c5dee7ffea639
Simon Christmann <simon@christmann.email>
2020-08-13 22:57:41 +0200

    First working proof-of-concept

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
 const {default: PQueue} = require('p-queue');
 const queue = new PQueue({concurrency: 1});
+const schedule = require('node-schedule');
 
 log.setLevel(config.verbosity);
 log.info(pkg.name + ' ' + pkg.version + ' starting');
@@ -40,7 +41,7 @@ log.debug('loaded config: ', config);
 const scopes = ['playlist-read-private', 'playlist-read-collaborative', 'playlist-modify-public', 'playlist-modify-private'];
 const redirectUri = `http://localhost:${config.port}/callback`;
 
-const spotifyApi = new SpotifyWebApi({
+const spotify = new SpotifyWebApi({
     redirectUri,
     clientId: config.clientId,
     clientSecret: config.clientSecret
@@ -52,52 +53,115 @@ const app = express();
 
 app.get('/login', (request, response) => {
     log.debug(request.params, request.body);
-    response.redirect(spotifyApi.createAuthorizeURL(scopes));
+    response.redirect(spotify.createAuthorizeURL(scopes));
 });
 
 app.get('/callback', (request, response) => {
-    const error = request.query.error;
-    const code = request.query.code;
-    const state = request.query.state;
-    log.debug('Callback', code, state);
-
-    if (error) {
+    if (request.query.error) {
         log.error('Callback Error:', error);
         response.send(`Callback Error: ${error}`);
         return;
     }
 
-    spotifyApi.authorizationCodeGrant(code).then(
-        data => {
-            const accessToken = data.body.access_token;
-            const refreshToken = data.body.refresh_token;
-            const expiresIn = data.body.expires_in;
+    const code = request.query.code;
+    const state = request.query.state;
+    log.debug('Callback', code, state);
 
-            spotifyApi.setAccessToken(accessToken);
-            spotifyApi.setRefreshToken(refreshToken);
+    spotify.authorizationCodeGrant(code).then(data => {
+        const accessToken = data.body.access_token;
+        const refreshToken = data.body.refresh_token;
+        const expiresIn = data.body.expires_in;
 
-            log.debug('access_token:', accessToken);
-            log.debug('refresh_token:', refreshToken);
+        spotify.setAccessToken(accessToken);
+        spotify.setRefreshToken(refreshToken);
 
-            log.info(`Sucessfully retreived access token. Expires in ${expiresIn} s.`);
-            response.send('Success! You can now close the window.');
+        log.debug('access_token:', accessToken);
+        log.debug('refresh_token:', refreshToken);
 
-            checkAuth().then(result => log.debug('Check auth:', result));
-        },
-        error => {
-            log.error('Error getting Tokens:', error);
-            response.send(`Error getting Tokens: ${error}`);
-        }
-    );
+        log.info(`Sucessfully retreived access token. Expires in ${expiresIn} s.`);
+        response.send('Success! You can now close the window.');
+
+        setInterval(async () => {
+            const data = await spotify.refreshAccessToken();
+            const access_token = data.body['access_token'];
+
+            console.log('The access token has been refreshed!');
+            console.log('access_token:', access_token);
+            spotify.setAccessToken(access_token);
+        }, expiresIn / 2 * 1000);
+
+        checkAuth().then(result => {
+            log.debug('Check auth:', result);
+
+            if (result) {
+                mainScheduler.invoke();
+            }
+        });
+    }).catch(error => {
+        log.error('Error getting Tokens:', error);
+        response.send(`Error getting Tokens: ${error}`);
+    });
 });
 
 app.listen(config.port, () => {
     log.info(`${config.name} listening on port ${config.port}`);
 });
 
+let savedState = [];
+const mainScheduler = schedule.scheduleJob('0 0 4 * * *', async () => {
+    try {
+        if (await checkAuth()) {
+            const myPlaylists = await spotify.getAllUserPlaylists();
+            const filteredPlaylists = myPlaylists.filter(p => /^Test/.test(p.name));
+            log.debug('Using playlists', filteredPlaylists.map(p => p.name));
+
+            const originalPlaylist = filteredPlaylists.find(p => /^Test$/.test(p.name));
+            const myPlaylist = filteredPlaylists.find(p => /\(save\)$/.test(p.name));
+            const blacklistPlaylist = filteredPlaylists.find(p => /\(deleted\)$/.test(p.name));
+
+            const tracksMine = (await spotify.getAllPlaylistTracks(myPlaylist.id)).map(t => t.track.uri);
+            log.debug('Your playlist', tracksMine);
+
+            // Get diff between locally saved state and "Playlist (save)", save to deleted playlist and get it
+            const deletedByMe = savedState.filter(x => !tracksMine.includes(x));
+            if (Array.isArray(deletedByMe) && deletedByMe.length) {
+                log.debug('You deleted', deletedByMe);
+                await spotify.addTracksToPlaylist(blacklistPlaylist.id, deletedByMe);
+            }
+            const tracksBlacklist = (await spotify.getAllPlaylistTracks(blacklistPlaylist.id)).map(t => t.track.uri);
+            log.debug('New Blacklist', tracksBlacklist);
+
+            // Gett source playlist tracks
+            const tracksOriginal = (await spotify.getAllPlaylistTracks(originalPlaylist.id)).map(t => t.track.uri);
+            log.debug('Source Playlist', tracksOriginal);
+
+            // Get new tracks, filter deleted/blacklisted tracks
+            const newTracks = tracksOriginal.filter(x => !tracksMine.includes(x));
+            log.debug('New tracks in source playlist', newTracks);
+            const newTracksWithoutDeleted = newTracks.filter(x => !tracksBlacklist.includes(x));
+
+            // Add new tracks to my playlist
+            if (Array.isArray(newTracksWithoutDeleted) && newTracksWithoutDeleted.length) {
+                log.debug('Adding new songs', newTracksWithoutDeleted);
+                await spotify.addTracksToPlaylist(myPlaylist.id, newTracksWithoutDeleted);
+            }
+
+            // Save my playlist for next run
+            savedState = (await spotify.getAllPlaylistTracks(myPlaylist.id)).map(t => t.track.uri);
+            log.debug('New saved state', savedState);
+
+        } else {
+            log.warn('Not authorized!');
+        }
+    } catch(error) {
+        log.error(error);
+    }
+});
+log.debug('scheduler', mainScheduler);
+
 async function checkAuth() {
     try {
-        await spotifyApi.getMe();
+        await spotify.getMe();
         return true;
     } catch {
         return false;
